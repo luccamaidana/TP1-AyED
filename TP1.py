40d75677a8ab6be5e6fe10ba96c695a2dcb50c4d
valenbutakera <132417906+valenbu03@users.noreply.github.com>
2023-05-03 01:28:03 -0300

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -17,5 +17,32 @@ minRub = 0
 indu = 'indumentaria'		
 perfu = "perfumería"		
 comi = "comida"		
-nombreLocal = " "		
+nombreLocal = " "	
+ubicacionLocal = " "
+salida = 0	
 
+def pantalla_locales():
+    print("Ingrese una opción a-d")
+    print("a- Crear locales")
+    print("b- Modificar local")
+    print("c- Eliminar local")
+    print("d- Volver")
+pantalla_locales()
+
+
+
+while (nombreLocal!= "*"):
+    mostrar_creacio()
+    nombreLocal = input("Ingrese el nombre")
+    ubicacionLocal = input("Ingrese la ubicacion")
+    select_rubro()
+comparacion_may()
+comparacion_min()
+exh_loc_may()
+exh_loc_min()
+print("Para salir pulse 0. Para volver pulse 1.")
+valid_salida()
+if salida == 0 :
+    Salir
+else:
+    Menu 
