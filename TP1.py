777182068cfc541f1b746588f058169430e114bb
luccamaidana <luccamaidana03@gmail.com>
2023-05-02 23:44:27 -0300

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -1,1 +1,22 @@
 #TP1
+
+#INICIO
+nombreUsuario = "admin@shopping.com"		
+claveUsuario = "12345"		
+password = " "		
+cont = 0		
+opc = 1		
+opcloc = " "		
+opcnov = " "		
+rub1 = 0		
+rub2 = 0		
+rub3 = 0		
+rubroLocal = 0		
+mayRub = 0		
+minRub = 0		
+indu = 'indumentaria'		
+perfu = "perfumería"		
+comi = "comida"		
+nombreLocal = " "		
+
+    
